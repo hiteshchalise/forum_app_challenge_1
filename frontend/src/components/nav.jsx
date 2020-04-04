98f9c26b52b151e65ef063903e0e9c070c65122a
Hitesh Chalise <hiteshchalise10@gmail.com>
2020-04-04 13:34:02 +0545

    working on frontend

---
+++ frontend/src/components/nav.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+  render() {
+    return (
+      <div id="main-nav">
+        <div className="app-name">
+          <h1>Forum App</h1>
+        </div>
+        <div className="nav-btn">
+          <button>Login</button>
+          <button>Register</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Nav;
