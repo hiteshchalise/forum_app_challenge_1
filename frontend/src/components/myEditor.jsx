84a6a9b794b77662fbf5d6d328429732f4134949
Hitesh Chalise <hiteshchalise10@gmail.com>
2020-04-24 18:10:12 +0545

    added superscript and blockquotes, need some work on blockquote i.e. when cursor is inside block we need to know so blockquote button will be in active state

--- frontend/src/components/myEditor.jsx
+++ frontend/src/components/myEditor.jsx
@@ -3,6 +3,23 @@ import { Editor, EditorState, RichUtils } from "draft-js";
 import "./style/myEditor.css"
 import { useEffect } from "react";
 
+
+const styleMap = {
+    'SUPERSCRIPT': {
+        position: "relative",
+        top: "-0.5em",
+        fontSize: "80%"
+    }
+}
+
+const blockStyleFn = (contentBlock) => {
+    const type = contentBlock.getType();
+    if (type === 'blockquote') {
+        return 'blockquote';
+    }
+}
+
+
 const MyEditor = (props) => {
     const [editorState, setEditorState] = useState(
         EditorState.createEmpty()
@@ -13,6 +30,8 @@ const MyEditor = (props) => {
     const [isUnderlined, setIsUnderlined] = useState(false);
     const [isStrikethrough, setIsStrikethrough] = useState(false);
     const [isCode, setIsCode] = useState(false);
+    const [isSuperscript, setIsSuperscript] = useState(false);
+    const [isBlockQuote, setIsBlockQuote] = useState(false);
 
 
     useEffect(() => {
@@ -23,6 +42,8 @@ const MyEditor = (props) => {
         setIsUnderlined(inlineStyle.has("UNDERLINE"))
         setIsStrikethrough(inlineStyle.has("STRIKETHROUGH"))
         setIsCode(inlineStyle.has("CODE"))
+        setIsSuperscript(inlineStyle.has("SUPERSCRIPT"))
+
     }, [editorState])
 
     const handleBold = () => {
@@ -50,10 +71,25 @@ const MyEditor = (props) => {
         setEditorState(RichUtils.toggleInlineStyle(editorState, 'CODE'))
     }
 
+    const handleSuperscript = () => {
+        setIsSuperscript(!isSuperscript);
+        setEditorState(RichUtils.toggleInlineStyle(editorState, 'SUPERSCRIPT'))
+    }
+
+    const handleBlockQuote = () => {
+        setIsBlockQuote(!isBlockQuote);
+        setEditorState(RichUtils.toggleBlockType(editorState, 'blockquote'))
+    }
+
     return (
         <div className="editor-container">
             <div className="main-section">
-                <Editor editorState={editorState} onChange={setEditorState} />
+                <Editor
+                    editorState={editorState}
+                    onChange={setEditorState}
+                    customStyleMap={styleMap}
+                    blockStyleFn={blockStyleFn}
+                />
             </div>
             <div className="footer-section">
                 <abbr title="Bold">
@@ -74,10 +110,22 @@ const MyEditor = (props) => {
                 <abbr title="Code">
                     <button onClick={handleCode} className={isCode ? "active" : "default"}><code>{"</>"}</code></button>
                 </abbr>
+                <abbr title="SuperScript">
+                    <button onClick={handleSuperscript} className={isSuperscript ? "active" : "default"}>A<sup>A</sup></button>
+                </abbr>
+                <abbr title="BlockQuote">
+                    <button onClick={handleBlockQuote} className={isBlockQuote ? "active" : "default"}><h2 >"</h2></button>
+                </abbr>
             </div>
             <big>hehe</big>
             <small>small</small>
             The <abbr title="World Health Organization">WHO</abbr> was founded in 1948.<em>Emphasized text</em> <p>He named his car <i>The lightning</i>, because it was very fast.</p> <p>This text contains <sup>superscript</sup> text.</p>
+            <br />
+            <blockquote >
+                For 50 years, WWF has been protecting the future of nature. The world's leading conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United States and close to 5 million globally.
+            </blockquote>
+
+            akka
         </div>
     )
 }
