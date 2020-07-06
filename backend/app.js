a2aad961a556b00fd061deaa12156c2681484574
Hitesh Chalise <hiteshchalise10@gmail.com>
2020-07-06 17:56:13 +0545

    clean up on app.js and index.js

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,10 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use("/api/posts", require("./route/posts"));
 app.use("/api/users", require("./route/users"));
 app.use("/api/auth", require("./route/auth"));
--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,6 @@ const app = require('./app');
 require('./mongoDB').connect();
 
 const PORT = process.env.PORT || 5000;
-
-const server = app.listen(
-  PORT,
-  () => console.log(`Server is running on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 module.exports = server;
