2aef74aef3d763683995240f2d77a2e616dbdb96
Hitesh Chalise <hiteshchalise10@gmail.com>
2022-06-02 11:02:23 +0545

    added responsive logged in user menu

--- reddet_frontend/src/components/LoggedInUserDisplay.tsx
+++ reddet_frontend/src/components/LoggedInUserDisplay.tsx
@@ -1,15 +1,18 @@
 import {
-  Text, Space,
+  Text,
   Avatar,
   Box,
   Menu,
+  Group,
 } from '@mantine/core';
 import { useAuth } from 'providers/authProvider';
-import { Logout } from 'tabler-icons-react';
+import { Link } from 'react-router-dom';
+import { Logout, Send } from 'tabler-icons-react';
 
 export interface ILoggedInUserDisplayProps {
   user: {
     name: string,
+    email: string
   }
 }
 
@@ -33,13 +36,41 @@ export default function LoggedInUserDisplay({ user }: ILoggedInUserDisplayProps)
       backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
       paddingLeft: theme.spacing.xs,
       paddingRight: theme.spacing.xs,
+      [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+        padding: 0,
+      },
     })}
     >
-      <Avatar radius="xl" color="red">{user.name.substring(0, 2)}</Avatar>
-      <Space w="sm" />
-
       <Menu
-        control={<Text size="sm" styles="bold">{user.name}</Text>}
+        control={(
+          <Group>
+            <Group
+              sx={(theme) => ({
+                [`@media (max-width: ${theme.breakpoints.xs}px)`]: {
+                  display: 'none',
+                },
+              })}
+            >
+              <Avatar radius="xl" color="red">{user.name.substring(0, 2)}</Avatar>
+              <div style={{ flex: 1 }}>
+                <Text size="sm" weight={500}>{user.name}</Text>
+                <Text size="xs" color="dimmed" lineClamp={1}>{user.email}</Text>
+              </div>
+            </Group>
+            <Avatar
+              sx={(theme) => ({
+                display: 'none',
+                [`@media (max-width: ${theme.breakpoints.xs}px)`]: {
+                  display: 'inline-block',
+                },
+              })}
+              radius="xl"
+              color="red"
+            >
+              {user.name.substring(0, 2)}
+            </Avatar>
+          </Group>
+        )}
         sx={{
           '&:Hover': {
             cursor: 'pointer',
@@ -51,6 +82,19 @@ export default function LoggedInUserDisplay({ user }: ILoggedInUserDisplayProps)
           {' '}
           {user.name}
         </Menu.Label>
+        <Menu.Item
+          icon={<Send size={24} color="gray" />}
+          sx={(theme) => ({
+            display: 'none',
+            [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+              display: 'inline-block',
+            },
+          })}
+          component={Link}
+          to="/postSubmit"
+        >
+          Add a post.
+        </Menu.Item>
         <Menu.Item
           icon={<Logout size={24} color="gray" />}
           onClick={handleLogout}
