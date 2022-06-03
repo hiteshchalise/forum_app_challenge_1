c9b5024c8f1e6c14a459aedbb6a3cd0cdd06a47e
Hitesh Chalise <hiteshchalise10@gmail.com>
2022-06-03 10:29:25 +0545

    cors changed to accept all

--- backend/startup/app.js
+++ backend/startup/app.js
@@ -1,19 +1,14 @@
 require('express-async-errors')
 const express = require('express')
-const path = require('path')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const middleware = require('../middleware/middleware')
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'build')))
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
+app.use(express.static('build'))
 
-
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use(middleware.requestLogger)
